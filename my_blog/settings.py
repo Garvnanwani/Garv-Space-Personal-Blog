3247b301fd282410051d513f1d0465e50f30902d
Garv nanwani <garvnanwani16@gmail.com>
2020-06-08 17:42:21 +0530

    .

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['garvnanwani.herokuapp.com', '127.0.0.1','0.0.0.0', 'localhost']
 
